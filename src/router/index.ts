397b60a85b0d16017702f2dd00dae3b0980e0ae3
xin.g <>
2025-11-04 20:55:04 +0800

    -

---
+++ src/router/index.ts
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const pages = import.meta.glob('../view/pages.ts', { eager: true, import: 'default' })
+// console.log(pages)
+
+interface RouteMeta {
+  name?: string
+  path: string
+  component: any
+}
+
+// console.log(Object.entries(pages))
+const routes = Object.entries(pages).flatMap(([path, meta]) => {
+  // console.log(meta)
+  return meta as RouteMeta[]
+})
+// console.log(routes)
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  // routes: [
+  //   {
+  //     name: 'Point',
+  //     path: '/',
+  //     component: () => import('@/view/point/index.vue'),
+  //   },
+  // ],
+})
+
+export default router
---
+++ src/view/pages.ts
@@ -0,0 +1,7 @@
+export default [
+  {
+    // name: 'Point',
+    path: '/',
+    component: () => import('@/view/point/index.vue'),
+  },
+]
